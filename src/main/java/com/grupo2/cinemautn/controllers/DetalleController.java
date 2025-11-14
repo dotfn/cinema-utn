581f0ad8ad4e5868420c9709a5523e4de7d404bb
Nicolas <169418539+dotfn@users.noreply.github.com>
2025-11-14 12:39:26 -0300

    Btn volver

--- src/main/java/com/grupo2/cinemautn/controllers/DetalleController.java
+++ src/main/java/com/grupo2/cinemautn/controllers/DetalleController.java
@@ -1,10 +1,16 @@
 package com.grupo2.cinemautn.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.controlsfx.control.Rating;
+
+import java.io.IOException;
 
 public class DetalleController {
 
@@ -34,5 +40,21 @@ public class DetalleController {
     @FXML
     public void initialize() {
         // Intencionalmente vacío: la vista es estática por ahora
+    }
+    @FXML
+    public void onCancelar(javafx.event.ActionEvent event) throws IOException {
+        // Navegar al dashboard
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/grupo2/cinemautn/fxml/dashboard.fxml"));
+        Scene mainScene = new Scene(loader.load());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(mainScene);
+        stage.setTitle("Principal");
+        stage.show();
+    }
+
+    @FXML
+    public void onBack(javafx.event.ActionEvent event) throws IOException {
+        onCancelar(event);
     }
 }
